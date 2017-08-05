3b86c0afdc0bec20d8993f21b6bc0a3e2ea63540
Ivan Busarov <Spaider629@gmail.com>
2017-08-05 15:10:11 +0300

    two pages with router

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,20 @@
 import * as React from "react";
 import { render } from "react-dom";
+import {
+    Route,
+    Link,
+    HashRouter as Router,
+} from "react-router-dom";
+
+const One = () => (<div><div>Page 1</div><Link to="/two">На страницу 2</Link></div>);
+const Two = () => (<div><div>Page 2</div><Link to="/">На страницу 1</Link></div>);
 
 render(
-    <div>
-        test
-    </div>,
+    <Router>
+        <div>
+            <Route exact path="/" component={One} />
+            <Route path="/two" component={Two} />
+        </div>
+    </Router>,
     document.getElementById("root"),
 );
